54a294ef7e254b2b61933c41a16128344744b024
Federico Carrillo Chaves <federico.carrilloch@alum.uca.es>
2019-11-04 18:28:44 +0100

    departments.py version 0.3

--- departments.py
+++ departments.py
@@ -6,23 +6,15 @@ import mobile
 import datetime
 from faker import Faker
 #   An alphanumeric code generator
-
-
-'''
-Here we have info about three plans, with their names, prices, minutes, etc...
-'''
-
 fake = Faker()
 
 
 def generate_departments():
     names = ["Sales", "Maintenance", "Support", "Anti-Cancellation"]
-    # we can use other representative names for them...
     name = random.choice(names)
 
     return [name]
-
-
+    
 deps = generate_departments()
 
 
