361cd03854a9653c492e383d235f1f0530e62057
zhanibek <janibek060639@gmail.com>
2019-11-04 19:10:29 +0100

    Added "generate_output" functions. The generator does not work yer, need to fix some little things.

--- departments.py
+++ departments.py
@@ -1,24 +1,11 @@
 import random
-import string
 import writer
 import general
-import mobile
-import datetime
-from faker import Faker
-#   An alphanumeric code generator
-fake = Faker()
-
-
-def generate_departments():
-    names = ["Sales", "Maintenance", "Support", "Anti-Cancellation"]
-    name = random.choice(names)
-
-    return [name]
-    
-deps = generate_departments()
 
 
 def generate_output():
+    names = ["Sales", "Maintenance", "Support", "Anti-Cancellation"]
+    name = random.choice(names)
     numeric_id = general.generate_id()
     departments = [numeric_id, name]
     writer.export_data(departments, "output/departments.csv")
