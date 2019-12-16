7d18b0f839717fcfdd0a6bf4bceccc536f6da10b
Federico Carrillo Chaves <federico.carrilloch@alum.uca.es>
2019-12-16 22:21:46 +0100

    name of table fixed

--- sql files/T0-T1/departments.sql
+++ sql files/T0-T1/departments.sql
@@ -1,8 +1,8 @@
-CREATE TABLE mytable(
-   id   VARCHAR(12) NOT NULL PRIMARY KEY
-  ,name VARCHAR(18) NOT NULL
+CREATE TABLE departments(
+   dept_id   VARCHAR(12) NOT NULL PRIMARY KEY
+  ,dept_name VARCHAR(18) NOT NULL
 );
-INSERT INTO mytable(id,name) VALUES ('DEPT_SALES','Sales');
-INSERT INTO mytable(id,name) VALUES ('DEPT_MAIN','Maintenance');
-INSERT INTO mytable(id,name) VALUES ('DEPT_SUPPORT','Support');
-INSERT INTO mytable(id,name) VALUES ('DEPT_CANCEL','Anti-Cancellation');
+INSERT INTO dbo.departments(dept_id,dept_name) VALUES ('DEPT_SALES','Sales');
+INSERT INTO dbo.departments(dept_id,dept_name) VALUES ('DEPT_MAIN','Maintenance');
+INSERT INTO dbo.departments(dept_id,dept_name) VALUES ('DEPT_SUPPORT','Support');
+INSERT INTO dbo.departments(dept_id,dept_name) VALUES ('DEPT_CANCEL','Anti-Cancellation');
