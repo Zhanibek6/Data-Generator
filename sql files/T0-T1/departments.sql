32d62e4e41af386ffd2b51f0af2512b5472edfa8
Federico Carrillo Chaves <federico.carrilloch@alum.uca.es>
2019-12-16 21:36:35 +0100

    adding .sql files based on our .csv files

---
+++ sql files/T0-T1/departments.sql
@@ -0,0 +1,8 @@
+CREATE TABLE mytable(
+   id   VARCHAR(12) NOT NULL PRIMARY KEY
+  ,name VARCHAR(18) NOT NULL
+);
+INSERT INTO mytable(id,name) VALUES ('DEPT_SALES','Sales');
+INSERT INTO mytable(id,name) VALUES ('DEPT_MAIN','Maintenance');
+INSERT INTO mytable(id,name) VALUES ('DEPT_SUPPORT','Support');
+INSERT INTO mytable(id,name) VALUES ('DEPT_CANCEL','Anti-Cancellation');
---
+++ sql files/T0-T1/internet_plans.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS internet_plans;
+CREATE TABLE IF NOT EXISTS internet_plans(
+   i_id              INTEGER  NOT NULL PRIMARY KEY IDENTITY(1,1)
+  ,i_name            VARCHAR(20) NOT NULL
+  ,i_bandwitdh       VARCHAR(10) NOT NULL
+  ,i_expiration_date INTEGER  NOT NULL
+  ,i_launch_date     INTEGER  NOT NULL
+);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (1,'200Mb','200',264,173);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (2,'500Mb','500',131,54);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (3,'500Mb','500',298,96);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (4,'1000Mb','1000',459,74);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (5,'1000Mb','1000',630,37);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (6,'1000Mb','1000',669,268);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (7,'500Mb','500',276,206);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (8,'1000Mb','1000',317,64);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (9,'500Mb','500',418,175);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (10,'500Mb','500',509,291);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (11,'200Mb','200',541,210);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (12,'1000Mb','1000',516,191);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (13,'200Mb','200',688,359);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (14,'1000Mb','1000',700,276);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (0,'200Mb','200',210,90);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (1,'200Mb','200',528,186);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (2,'1000Mb','1000',259,191);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (3,'1000Mb','1000',483,232);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (4,'1000Mb','1000',476,280);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (5,'200Mb','200',105,19);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (6,'1000Mb','1000',458,37);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (7,'1000Mb','1000',344,232);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (8,'1000Mb','1000',396,275);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (9,'1000Mb','1000',596,183);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (10,'200Mb','200',618,258);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (11,'500Mb','500',560,110);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (12,'200Mb','200',628,234);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (13,'200Mb','200',717,179);
+INSERT INTO internet_plans(i_id,i_name,i_bandwitdh,i_expiration_date,i_launch_date) VALUES (14,'200Mb','200',457,355);
