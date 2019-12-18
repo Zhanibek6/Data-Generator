2f40d74e01b203ebce54a6a147e52272dbe26d7d
zhanibek <janibek060639@gmail.com>
2019-12-18 12:08:45 +0100

    Now old clients

--- internet.py
+++ internet.py
@@ -5,7 +5,7 @@ import general
 import mobile
 import datetime
 from faker import Faker
-#   An alphanumeric code generator
+import settings
 
 
 '''
@@ -21,7 +21,7 @@ def generate_internet_plan():
     # we can use other representative names for them...
     prices = [90, 150, 300]  # PLN
     minutes = [200, 400, -1]  # -1 means unlimited
-    bandwidth = [200, 500, 1000]  # in Mb/s
+    bandwidth = ["low", "medium", "high"]  # in Mb/s
     launch_date = [datetime.date(2016, 6, 6), datetime.date(2017, 7, 7), datetime.date(2018, 8, 8)]
     expiration_date = [datetime.date(2018, 6, 6), datetime.date(2018, 7, 7), datetime.date(2018, 8, 8)]
     name = random.choice(names)
@@ -55,7 +55,7 @@ def generate_output(i_id):
     expiration_date = random.randint(contract_date, 1400)
     # expiration_date = mobile.fake.date_between(start_date=contract_date, end_date='now')
     internet_plan = [i_id+14, plan[0], plan[1], expiration_date, contract_date]
-    writer.export_data(internet_plan, "output/T1-T2/internet_plans.csv")
+    writer.export_data(internet_plan, settings.location+"internet_plans.csv")
 
 
 t_one = datetime.date(2018, 1, 1)
@@ -75,21 +75,30 @@ def client_output():
 
 
 def having_ip():
-    import client
-    phone = client.generate_phone()
-    i_id = random.randint(14, 20)
-    cli_id = random.randint(1000, 2000)
+    old_chance = random.randint(1, 100)
+    if old_chance < 30:
+        client = writer.get_row("output/T0-T1/having_ip.csv",
+                                random.randint(1, writer.count_row("output/T0-T1/having_ip.csv") - 1))
+        phone = client[0]
+        cli_id = client[1]
+    else:
+        client = writer.get_row(settings.location + "client.csv",
+                                random.randint(1, writer.count_row(settings.location + "client.csv") - 1))
+        phone = client[7]
+        cli_id = client[0]
+    ip_plan = writer.get_row(settings.location + "internet_plans.csv",
+                             random.randint(1, writer.count_row(settings.location + "internet_plans.csv") - 1))
+    i_id = ip_plan[0]
     contract = random.randint(365, 720)
-    writer.get_row("output/T0-T1/internet_plans.csv", i_id)
-    mp = [phone,
+    ip = [phone,
           cli_id,  # The id itself
-          i_id+1000,
+          i_id,
           contract,  # The date
           random.randint(contract, 1400),  # Permanence
           random.choice([100, 150, 200, 250, 300, 350, 400]),  # Price
           random.choice([" ", 300, 400, 500]),  # Minutes
           ]
-    writer.export_data(mp, "output/T1-T2/having_ip.csv")
+    writer.export_data(ip, settings.location+"having_ip.csv")
 
 
 for i in range(1000):
